cb00d84b00a2c05df8b4ba3031e7cbd7eca62d50
ariesharry <ariesharry3@gmail.com>
2023-03-30 16:45:34 +0700

    add body parser package

--- restapi/app.js
+++ restapi/app.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const app = express();
-const auctionRoutes = require('./api/routes/auction');
-const bidRoutes = require('./api/routes/bid');
 const morgan = require('morgan');
 
+const auctionRoutes = require('./api/routes/auction');
+const bidRoutes = require('./api/routes/bid');
+
+app.use(morgan('dev'));
 app.use('/auction', auctionRoutes);
 app.use('/bid', bidRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found!');
+    error.status= 404;
+    next(error);
+
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+
+    });
+
+});
 
 module.exports = app;

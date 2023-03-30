f0bcaa509f7679b91696f8a87c19e7ce0ade5d7a
ariesharry <ariesharry3@gmail.com>
2023-03-30 08:19:26 +0700

    init

---
+++ front-end/src/layouts/dashboard/config.js
@@ -0,0 +1,86 @@
+import ChartBarIcon from '@heroicons/react/24/solid/ChartBarIcon';
+import CogIcon from '@heroicons/react/24/solid/CogIcon';
+import LockClosedIcon from '@heroicons/react/24/solid/LockClosedIcon';
+import ShoppingBagIcon from '@heroicons/react/24/solid/ShoppingBagIcon';
+import UserIcon from '@heroicons/react/24/solid/UserIcon';
+import UserPlusIcon from '@heroicons/react/24/solid/UserPlusIcon';
+import UsersIcon from '@heroicons/react/24/solid/UsersIcon';
+import PlusIcon from '@heroicons/react/24/solid/PlusIcon';
+import XCircleIcon from '@heroicons/react/24/solid/XCircleIcon';
+import { SvgIcon } from '@mui/material';
+import BuildingOffice2Icon from '@heroicons/react/24/solid/BuildingOffice2Icon'
+
+export const items = [
+  {
+    title: 'Dashboard',
+    path: '/',
+    icon: (
+      <SvgIcon fontSize="small">
+        <ChartBarIcon />
+      </SvgIcon>
+    )
+  },
+  {
+    title: 'Add Commodity',
+    path: '/customers',
+    icon: (
+      <SvgIcon fontSize="small">
+        <PlusIcon />
+      </SvgIcon>
+    )
+  },
+  {
+    title: 'My Auction',
+    path: '/companies',
+    icon: (
+      <SvgIcon fontSize="small">
+        <ShoppingBagIcon />
+      </SvgIcon>
+    )
+  },
+  {
+    title: 'Account',
+    path: '/account',
+    icon: (
+      <SvgIcon fontSize="small">
+        <UserIcon />
+      </SvgIcon>
+    )
+  },
+  {
+    title: 'Settings',
+    path: '/settings',
+    icon: (
+      <SvgIcon fontSize="small">
+        <CogIcon />
+      </SvgIcon>
+    )
+  },
+  {
+    title: 'Login',
+    path: '/auth/login',
+    icon: (
+      <SvgIcon fontSize="small">
+        <LockClosedIcon />
+      </SvgIcon>
+    )
+  },
+  {
+    title: 'Register',
+    path: '/auth/register',
+    icon: (
+      <SvgIcon fontSize="small">
+        <UserPlusIcon />
+      </SvgIcon>
+    )
+  },
+  {
+    title: 'PKS Profile',
+    path: '/404',
+    icon: (
+      <SvgIcon fontSize="small">
+        <BuildingOffice2Icon />
+      </SvgIcon>
+    )
+  }
+];
---
+++ front-end/src/sections/commodity/commodity-form.js
@@ -0,0 +1,257 @@
+import { useCallback, useState } from 'react';
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Divider,
+  TextField,
+  Unstable_Grid2 as Grid
+} from '@mui/material';
+
+const states = [
+  {
+    value: 'alabama',
+    label: 'Alabama'
+  },
+  {
+    value: 'new-york',
+    label: 'New York'
+  },
+  {
+    value: 'san-francisco',
+    label: 'San Francisco'
+  },
+  {
+    value: 'los-angeles',
+    label: 'Los Angeles'
+  }
+];
+
+export const CommodityForm = () => {
+  const [values, setValues] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    state: 'banten',
+    country: ''
+  });
+
+  const handleChange = useCallback(
+    (event) => {
+      setValues((prevState) => ({
+        ...prevState,
+        [event.target.name]: event.target.value
+      }));
+    },
+    []
+  );
+
+  const handleSubmit = useCallback(
+    (event) => {
+      event.preventDefault();
+    },
+    []
+  );
+
+  return (
+    <form
+      autoComplete="off"
+      noValidate
+      onSubmit={handleSubmit}
+    >
+      <Card>
+        <CardHeader
+          subheader="Add new commodity"
+          title="Add Commodity"
+        />
+        <CardContent sx={{ pt: 0 }}>
+          <Box sx={{ m: -1.5 }}>
+            <Grid
+              container
+              spacing={3}
+            >
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  helperText="Enter the commodity (ex: CPO)"
+                  label="Commodity"
+                  name="commodity"
+                  onChange={handleChange}
+                  required
+                  value={values.firstName}
+                />
+              </Grid>
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  label="Owner"
+                  name="owner"
+                  onChange={handleChange}
+                  required
+                  value={values.lastName}
+                />
+              </Grid>
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  label="Lot number"
+                  name="lotNumber"
+                  onChange={handleChange}
+                  required
+                  value={values.email}
+                />
+              </Grid>
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  label="Quality"
+                  name="quality"
+                  onChange={handleChange}
+                  type="number"
+                  value={values.phone}
+                />
+              </Grid>
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  label="Quantity"
+                  name="quantity"
+                  onChange={handleChange}
+                  required
+                  value={values.country}
+                />
+              </Grid>
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  label="Price"
+                  name="price"
+                  onChange={handleChange}
+                  required
+                  value={values.country}
+                />
+              </Grid>
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  label="Factory"
+                  name="factory"
+                  onChange={handleChange}
+                  required
+                  value={values.country}
+                />
+              </Grid>
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  label="Currency"
+                  name="currency"
+                  onChange={handleChange}
+                  required
+                  value= "IDR"
+                />
+              </Grid>
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  label="Shippment"
+                  name="shippment"
+                  onChange={handleChange}
+                  required
+                  value={values.country}
+                />
+              </Grid>
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  label="Port of Loading"
+                  name="portOfLoading"
+                  onChange={handleChange}
+                  required
+                  value={values.country}
+                />
+              </Grid>
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  label="Delivery date"
+                  name="deliveryDate"
+                  onChange={handleChange}
+                  required
+                  value={values.country}
+                />
+              </Grid>
+              {/* <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  label="Select State"
+                  name="state"
+                  onChange={handleChange}
+                  required
+                  select
+                  SelectProps={{ native: true }}
+                  value={values.state}
+                >
+                  {states.map((option) => (
+                    <option
+                      key={option.value}
+                      value={option.value}
+                    >
+                      {option.label}
+                    </option>
+                  ))}
+                </TextField>
+              </Grid> */}
+            </Grid>
+          </Box>
+        </CardContent>
+        <Divider />
+        <CardActions sx={{ justifyContent: 'flex-end' }}>
+          <Button variant="contained">
+            Submit
+          </Button>
+        </CardActions>
+      </Card>
+    </form>
+  );
+};

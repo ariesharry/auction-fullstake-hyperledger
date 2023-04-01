aaaee841b2eccf6025901c7b3176af805cbe0218
ariesharry <ariesharry3@gmail.com>
2023-04-01 17:37:11 +0700

    add query to dashboard

--- front-end/src/sections/overview/overview-latest-orders.js
+++ front-end/src/sections/overview/overview-latest-orders.js
@@ -1,6 +1,8 @@
 import { format } from 'date-fns';
 import PropTypes from 'prop-types';
 import ArrowRightIcon from '@heroicons/react/24/solid/ArrowRightIcon';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import {
   Box,
   Button,
@@ -25,7 +27,92 @@ const statusMap = {
 };
 
 export const OverviewLatestOrders = (props) => {
-  const { orders = [], sx } = props;
+
+  const [dataAuction, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.post('http://localhost:3001/queryAuction', {
+          org: 'org1',
+          user: 'ptpn',
+          auctionID: 'auction1'
+        });
+        setData([response.data.auctionDetails]);
+        // console.log(auctionData);
+        // console.log(auctionData.item);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, [] );
+
+  console.log(dataAuction[0]['item']);
+
+  const orders = [
+    {
+      id: 'f69f88012978187a6c12897f',
+      ref: 'auction1',
+      amount: 30.5,
+      customer: {
+        name: dataAuction[0]['item']
+      },
+      createdAt: 1555016400000,
+      status: dataAuction[0]['status']
+    },
+    {
+      id: '9eaa1c7dd4433f413c308ce2',
+      ref: 'DEV1048',
+      amount: 25.1,
+      customer: {
+        name: 'Cao Yu'
+      },
+      createdAt: 1555016400000,
+      status: 'delivered'
+    },
+    {
+      id: '01a5230c811bd04996ce7c13',
+      ref: 'DEV1047',
+      amount: 10.99,
+      customer: {
+        name: 'Alexa Richardson'
+      },
+      createdAt: 1554930000000,
+      status: 'refunded'
+    },
+    {
+      id: '1f4e1bd0a87cea23cdb83d18',
+      ref: 'DEV1046',
+      amount: 96.43,
+      customer: {
+        name: 'Anje Keizer'
+      },
+      createdAt: 1554757200000,
+      status: 'pending'
+    },
+    {
+      id: '9f974f239d29ede969367103',
+      ref: 'DEV1045',
+      amount: 32.54,
+      customer: {
+        name: 'Clarke Gillebert'
+      },
+      createdAt: 1554670800000,
+      status: 'delivered'
+    },
+    {
+      id: 'ffc83c1560ec2f66a1c05596',
+      ref: 'DEV1044',
+      amount: 16.76,
+      customer: {
+        name: 'Adam Denisov'
+      },
+      createdAt: 1554670800000,
+      status: 'delivered'
+    }
+  ]
+  const { sx } = props;
 
   return (
     <Card sx={sx}>

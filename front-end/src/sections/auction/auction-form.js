f76d94c38330a2e718f747c4245bc44390b2b577
ariesharry <ariesharry3@gmail.com>
2023-04-01 16:28:37 +0700

    add new network

--- front-end/src/sections/auction/auction-form.js
+++ front-end/src/sections/auction/auction-form.js
@@ -1,4 +1,8 @@
 import { useCallback, useState } from 'react';
+import axios from 'axios';
+import Snackbar from '@mui/material/Snackbar';
+import * as React from 'react';
+import MuiAlert from '@mui/material/Alert';
 import {
   Box,
   Button,
@@ -32,13 +36,30 @@ const states = [
 
 export const CommodityForm = () => {
   const [values, setValues] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
-    state: 'banten',
-    country: ''
+    org: '',
+    user: '',
+    auctionID: '',
+    item: '',
+    quantity: 0
   });
+
+  const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
 
   const handleChange = useCallback(
     (event) => {
@@ -56,6 +77,19 @@ export const CommodityForm = () => {
     },
     []
   );
+
+  const addAuction = (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:3001/auction', {
+      org: values.org,
+      user: values.user,
+      auctionID: values.auctionID,
+      item: values.item,
+      quantity: values.quantity,
+      auditor: "withAuditor"
+    }).then(res => console.log("adding new auction", res)).catch(err => console.log(err));
+    handleClick();
+  };
 
   return (
     <form
@@ -80,12 +114,38 @@ export const CommodityForm = () => {
               >
                 <TextField
                   fullWidth
-                  helperText="Enter the commodity (ex: CPO)"
-                  label="Commodity"
-                  name="commodity"
+                  label="Auction ID/Lot number"
+                  name="auctionID"
                   onChange={handleChange}
                   required
-                  value={values.firstName}
+                  value={values.auctionID}
+                />
+              </Grid>
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  helperText="Enter the commodity (ex: CPO)"
+                  label="Commodity"
+                  name="item"
+                  onChange={handleChange}
+                  required
+                  value={values.item}
+                />
+              </Grid>
+              <Grid
+                xs={12}
+                md={6}
+              >
+                <TextField
+                  fullWidth
+                  label="Group"
+                  name="org"
+                  onChange={handleChange}
+                  required
+                  value={values.org}
                 />
               </Grid>
               <Grid
@@ -95,25 +155,13 @@ export const CommodityForm = () => {
                 <TextField
                   fullWidth
                   label="Owner"
-                  name="owner"
+                  name="user"
                   onChange={handleChange}
                   required
-                  value={values.lastName}
+                  value={values.user}
                 />
               </Grid>
-              <Grid
-                xs={12}
-                md={6}
-              >
-                <TextField
-                  fullWidth
-                  label="Lot number"
-                  name="lotNumber"
-                  onChange={handleChange}
-                  required
-                  value={values.email}
-                />
-              </Grid>
+              
               <Grid
                 xs={12}
                 md={6}
@@ -123,8 +171,7 @@ export const CommodityForm = () => {
                   label="Quality"
                   name="quality"
                   onChange={handleChange}
-                  type="number"
-                  value={values.phone}
+                  // value={values.phone}
                 />
               </Grid>
               <Grid
@@ -137,7 +184,8 @@ export const CommodityForm = () => {
                   name="quantity"
                   onChange={handleChange}
                   required
-                  value={values.country}
+                  type="number"
+                  value={values.quantity}
                 />
               </Grid>
               <Grid
@@ -150,7 +198,7 @@ export const CommodityForm = () => {
                   name="price"
                   onChange={handleChange}
                   required
-                  value={values.country}
+                  // value={values.country}
                 />
               </Grid>
               <Grid
@@ -163,7 +211,7 @@ export const CommodityForm = () => {
                   name="factory"
                   onChange={handleChange}
                   required
-                  value={values.country}
+                  // value={values.country}
                 />
               </Grid>
               <Grid
@@ -189,7 +237,7 @@ export const CommodityForm = () => {
                   name="shippment"
                   onChange={handleChange}
                   required
-                  value={values.country}
+                  // value={values.country}
                 />
               </Grid>
               <Grid
@@ -202,7 +250,7 @@ export const CommodityForm = () => {
                   name="portOfLoading"
                   onChange={handleChange}
                   required
-                  value={values.country}
+                  // value={values.country}
                 />
               </Grid>
               <Grid
@@ -215,7 +263,7 @@ export const CommodityForm = () => {
                   name="deliveryDate"
                   onChange={handleChange}
                   required
-                  value={values.country}
+                  // value={values.country}
                 />
               </Grid>
               {/* <Grid
@@ -247,9 +295,17 @@ export const CommodityForm = () => {
         </CardContent>
         <Divider />
         <CardActions sx={{ justifyContent: 'flex-end' }}>
-          <Button variant="contained">
+          <Button
+          variant="contained"
+          loading={true}
+          onClick={addAuction}>
             Add Auction
           </Button>
+          <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+            <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+              Successfully added a new auction!
+            </Alert>
+          </Snackbar>
         </CardActions>
       </Card>
     </form>

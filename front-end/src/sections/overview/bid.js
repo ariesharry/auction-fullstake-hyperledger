8d6541eeaa0cfe389ca031c1d9a42c9ec21369e6
Aries Harry Pratama <ariesharry3@gmail.com>
2023-04-30 06:02:07 +0000

    remove textfield

--- front-end/src/sections/overview/bid.js
+++ front-end/src/sections/overview/bid.js
@@ -7,9 +7,78 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import { InputAdornment } from '@mui/material';
+import { Descriptions } from 'antd';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import MuiAlert from '@mui/material/Alert';
+import Snackbar from '@mui/material/Snackbar';
 
 export default function FormDialog() {
   const [open, setOpen] = React.useState(false);
+  const [openAlert, setOpenAlert] = React.useState(false);
+  const [openAlertSubmit, setOpenAlertSubmit] = React.useState(false);
+  const [dataID, setDataID] = useState([]);
+  // const [dataAuction, setData] = useState([{
+  //   id: 'auction1',
+  //   seller: 'PTPN',
+  //   quantity: '10000',
+  //   item: 'CPO',
+  //   createdAt: 1555016400000,
+  //   status: 'Open'
+  // }]);
+
+  // const queryAuction = () => {
+  //   // e.preventDefault();
+  //   axios.post('http://103.250.10.234:3001/queryAuction', {
+  //     org: "org2",
+  //     user: "bidder1",
+  //     auctionID: "00001"
+  //   }).then(res => setData([res.data.auctionCreated])).catch(err => console.log(err));
+  // };
+
+  const handleClickAlert = () => {
+    setOpenAlert(true);
+  };
+
+  const handleCloseAlert = () => {
+    setOpenAlert(false);
+  };
+
+  const handleClickAlertSubmit = () => {
+    setOpenAlertSubmit(true);
+  };
+
+  const handleCloseAlertSubmit = () => {
+    setOpenAlertSubmit(false);
+  };
+
+  const addBid = () => {
+    // e.preventDefault();
+    axios.post('http://103.250.10.234:3001/bid', {
+      org: "org2",
+      user: "bidder1",
+      auctionID: "00003",
+      quantity: 2000,
+      price: 10000
+    }).then(res => setDataID([res.data.auctionCreated])).catch(err => console.log(err));
+    handleClickAlert();
+  };
+
+  const submitBid = () => {
+    // e.preventDefault();
+    axios.post('http://103.250.10.234:3001/submitBid', {
+      org: "org2",
+      user: "bidder1",
+      auctionID: "00003",
+      bidID: 2000
+    }).then(res => setDataID([res.data.auctionCreated])).catch(err => console.log(err));
+    handleClickAlertSubmit();
+  };
+
+  console.log(dataID);
+
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -18,6 +87,10 @@ export default function FormDialog() {
   const handleClose = () => {
     setOpen(false);
   };
+
+  const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
 
   return (
     <div>
@@ -35,6 +108,7 @@ export default function FormDialog() {
           <TextField
           label="Price"
           id="price"
+          name='price'
           type="number"
           sx={{ m: 1, width: '25ch' }}
           variant="outlined"
@@ -46,6 +120,7 @@ export default function FormDialog() {
           <TextField
           label="Quantity"
           id="quantity"
+          name='quantity'
           type="number"
           sx={{ m: 1, width: '25ch' }}
           variant="outlined"
@@ -77,8 +152,20 @@ export default function FormDialog() {
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Subscribe</Button>
+          <Button variant="outlined" onClick={handleClickAlert}>Place Bid</Button>
+          <Button variant="contained" onClick={submitBid}>Submit Bid</Button>
+          
         </DialogActions>
+        <Snackbar open={openAlert} autoHideDuration={6000} onClose={handleCloseAlert}>
+          <Alert onClose={handleCloseAlert} severity="success" sx={{ width: '100%' }}>
+            Successfully place bid!
+          </Alert>
+        </Snackbar>
+        <Snackbar open={openAlertSubmit} autoHideDuration={6000} onClose={function(event){handleClose(); handleCloseAlertSubmit()}}>
+          <Alert onClose={function(event){handleClose(); handleCloseAlertSubmit()}} severity="success" sx={{ width: '100%' }}>
+            Successfully submit auction!
+          </Alert>
+        </Snackbar>
       </Dialog>
     </div>
   );
